6b48f96c34c367ead23a464ecb8f1a871ccf41b8
PhuNguyen8903 <ngngocphu8903@gmail.com>
2026-09-16 10:57:57 +0700

    solving cookie error

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
+        domain: process.env.COOKIE_DOMAIN,
+        sameSite: "strict",
         secure: config.env === "production",
         httpOnly: true,
         maxAge: 1 * 60 * 60 * 1000, // khop voi expiration 
