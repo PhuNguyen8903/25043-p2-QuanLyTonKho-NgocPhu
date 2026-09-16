8721d3547f4609a0fdc2dacf4c429758280c0dad
PhuNguyen8903 <ngngocphu8903@gmail.com>
2026-09-16 08:54:04 +0700

    add required file

--- backend/model/index.js
+++ backend/model/index.js
@@ -1,20 +1,21 @@
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + "/../config/config.js")[env]
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username,config.password,config)
+const sequelize = new Sequelize(config.database, config.username, config.password, { ...config, dialectModule: mysql });
 
 //khởi tạo db
-db.User =require("./user")(sequelize,Sequelize)
-db.Product = require("./product")(sequelize,Sequelize)
-db.Supplier = require("./supplier")(sequelize,Sequelize)
+db.User = require("./user")(sequelize, Sequelize)
+db.Product = require("./product")(sequelize, Sequelize)
+db.Supplier = require("./supplier")(sequelize, Sequelize)
 db.PurchaseOrder = require("./purchaseOrder")(sequelize, Sequelize)
 db.PurchaseOrderItem = require("./purchaseOrderItem")(sequelize, Sequelize)
 db.SalesOrder = require("./salesOrder")(sequelize, Sequelize)
 db.SalesOrderItem = require("./salesOrderItem")(sequelize, Sequelize)
-Object.keys(db).forEach(modelName =>{
-    if(db[modelName].associate){
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
         db[modelName].associate(db);
     }
 })
